85340e76d04b4547e4f0e89e6f3623c7cc5c5280
Alexcaussades <alexcaussades@gmail.com>
2021-06-12 09:57:21 +0200

    change form

--- views/home/index.js
+++ views/home/index.js
@@ -1,11 +1,20 @@
 const {app, BrowserWindow, ipcMain} = require('electron');
+const webPreferences = require('../../webPreferences')
 //const {dialog, Menu, MenuItem, getCurrentWindow} = require('electron').remote;
 const {data: ivaoData} = require("../../ivao/api-ivao.json")
 const $ = require('jquery')
 const fetch = require('node-fetch');
+const version = require('../../logs-data/version.json')
+
+
+
 
 
 $('#result').hide()
+
+$('#version').html('Version: ' + version.data.version)
+
+
 
 $('#platforme').on('click', () => {
   const metar = ivaoData.metar
